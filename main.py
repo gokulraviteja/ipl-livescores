fe67f66076b4fcbee211b779d198f3b6d3e6cb09
Gokul Ravi Teja <bgokulravi1999@gmail.com>
2020-10-05 21:38:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+print("I am here")
 driver.get("https://www.cricbuzz.com/live-cricket-scores/30409/rcb-vs-dc-19th-match-indian-premier-league-2020")
 
 for i in range(5):
